f352a52b83c3797ef8986498a5b6709b7be34b6d
Noercholis <poetnoer@gmail.com>
2016-04-10 13:18:28 +0700

    fix namespace

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 var extend 	= require('extend');
 
-var  nuWidget_counter = 0,
-    nuWidget_objects = {};
-var nu = nu || {};
+exports.nuWidget_counter    = exports.nuWidget_counter || 0;
+exports.nuWidget_objects     = exports.nuWidget_objects || {};
 
 exports.widget = function (widget_name,base) {
     var namespace           = widget_name.split('.')[0];
     var widget              = widget_name.split('.')[1];
-    nu[namespace]           = nu[namespace]||{};
-    nu[namespace][widget]    = function(){};
-    extend(true,nu[namespace][widget].prototype,base);
+    exports[namespace]      = exports[namespace]||{};
+    exports[namespace][widget]   = function(){};
+    extend(true,exports[namespace][widget].prototype,base);
     exports[widget]            = function(){
         var args = [];
         Array.prototype.push.apply( args, arguments );
@@ -17,16 +16,16 @@ exports.widget = function (widget_name,base) {
         var widget_id 	= 'nuWidget_' + widget;
         switch(typeof options){
             case 'object':
-                nuWidget_counter++;
-                nuWidget_objects[widget_id] = extend(true,{},nu[namespace][widget].prototype);
-                extend(true,nuWidget_objects[widget_id],{
+                exports.nuWidget_counter++;
+                exports.nuWidget_objects[widget_id] = extend(true,{},exports[namespace][widget].prototype);
+                extend(true,exports.nuWidget_objects[widget_id],{
                     options : options,
                 });
-                nuWidget_objects[widget_id]._create();
+                exports.nuWidget_objects[widget_id]._create();
                 break;
             case 'string':
-                if(typeof nuWidget_objects[widget_id]!='undefined')
-                    nuWidget_objects[widget_id][options].apply(nuWidget_objects[widget_id],args);
+                if(typeof exports.nuWidget_objects[widget_id]!='undefined')
+                    exports.nuWidget_objects[widget_id][options].apply(exports.nuWidget_objects[widget_id],args);
                 break;
         }
         return this;

3b8e991b873fa7e70aac460c0a925bacdd660187
Noercholis <poetnoer@gmail.com>
2017-04-01 12:01:33 +0700

    fix extend

--- nu-widget.js
+++ nu-widget.js
@@ -1,15 +1,15 @@
 var extend 	= require('extend');
 
-exports.nuWidget_counter    = exports.nuWidget_counter || 0;
-exports.nuWidget_objects     = exports.nuWidget_objects || {};
-
 global.nu = {
+    nuWidget_counter : 0,
+    nuWidget_objects : {},
     widget : function (widget_name,base) {
+        var self                    = this;
         var namespace               = widget_name.split('.')[0];
         var widget                  = widget_name.split('.')[1];
-        exports[namespace]          = exports[namespace]||{};
-        exports[namespace][widget]  = function(){};
-        extend(true,exports[namespace][widget].prototype,base);
+        self[namespace]             = self[namespace]||{};
+        self[namespace][widget]     = function(){};
+        extend(true,self[namespace][widget].prototype,base);
         this[widget]                = function(){
             var args = [];
             Array.prototype.push.apply( args, arguments );
@@ -17,27 +17,23 @@ global.nu = {
             var widget_id   = 'nuWidget_' + widget;
             switch(typeof options){
                 case 'object':
-                    exports.nuWidget_counter++;
-                    exports.nuWidget_objects[widget_id] = extend(true,{},exports[namespace][widget].prototype);
-                    extend(true,exports.nuWidget_objects[widget_id],{
+                    self.nuWidget_counter++;
+                    self.nuWidget_objects[widget_id] = extend(true,{},self[namespace][widget].prototype);
+                    extend(true,self.nuWidget_objects[widget_id],{
                         options : options,
                     });
-                    exports.nuWidget_objects[widget_id]._create();
-                    return exports.nuWidget_objects[widget_id];
+                    self.nuWidget_objects[widget_id]._create();
+                    return self.nuWidget_objects[widget_id];
                     break;
                 case 'string':
-                    if(typeof exports.nuWidget_objects[widget_id]!='undefined')
-                        exports.nuWidget_objects[widget_id][options].apply(exports.nuWidget_objects[widget_id],args);
+                    if(typeof self.nuWidget_objects[widget_id]!='undefined')
+                        self.nuWidget_objects[widget_id][options].apply(self.nuWidget_objects[widget_id],args);
                     break;
             }
-            return exports.nuWidget_objects[widget_id];
+            return self.nuWidget_objects[widget_id];
         }
+    },
+    extend : function(){
+        extend.apply(this,arguments);
     }
-}
-
-var base = {
-    extend : extend,
-}
-exports.extend = function(){
-    base.extend.apply(this,arguments);
 }
